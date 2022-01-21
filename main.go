0fb7435c2e6c3889c69586ede76709f76fe719ab
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-22 00:11:40 +0900

    update

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func total(n int, c chan int) {
+func total(c chan int) {
+	n := <-c
+	fmt.Println("n =", n)
 	t := 0
 	for i := 1; i <= n; i++ {
 		t += i
 	}
-	c <- t
+	fmt.Println("total", t)
 }
 
 func main() {
 	c := make(chan int)
-	go total(100, c)
-	fmt.Println("total", <-c)
+	go total(c)
+	c <- 100
+	time.Sleep(10 * time.Millisecond)
 }
