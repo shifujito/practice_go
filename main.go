3013a3aa96a50fddf0c62e82fc458a0b9a60d192
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-22 12:26:15 +0900

    update

--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func total(cs chan int, cr chan int) {
-	n := <-cs
-	fmt.Println("n = ", n)
-	t := 0
-	for i := 1; i <= n; i++ {
-		t += i
-	}
-	cr <- t
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	cs := make(chan int)
-	cr := make(chan int)
-	go total(cs, cr)
-	cs <- 100
-	fmt.Println("total:", <-cr)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServer", nil)
+	}
 }
