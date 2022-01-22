8376918f4e5fba0f9e2bf8b64db2ef0cbe0a2d45
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-22 16:04:24 +0900

    update

---
+++ room.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: messageBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, reeq *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+
+}
