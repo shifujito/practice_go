f4ad77bb095c373a353eb7a6edf2d11521ab1ca3
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-22 12:05:41 +0900

    update

--- main.go
+++ main.go
@@ -2,33 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"time"
 )
 
-func prmsg(n int, s string) {
-	fmt.Println(s)
-	time.Sleep(time.Duration(n) * time.Millisecond)
-}
-
-func first(n int, c chan string) {
-	const nm string = "first-"
-	for i := 0; i < 10; i++ {
-		s := nm + strconv.Itoa(i)
-		prmsg(n, s)
-		c <- s
+func total(cs chan int, cr chan int) {
+	n := <-cs
+	fmt.Println("n = ", n)
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
 	}
-}
-
-func second(n int, c chan string) {
-	for i := 0; i < 10; i++ {
-		prmsg(n, "second:["+<-c+"]")
-	}
+	cr <- t
 }
 
 func main() {
-	c := make(chan string)
-	go first(10, c)
-	second(10, c)
-	fmt.Println()
+	cs := make(chan int)
+	cr := make(chan int)
+	go total(cs, cr)
+	cs <- 100
+	fmt.Println("total:", <-cr)
 }
