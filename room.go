d1ac880c1c346224fe3aec957e55af455569010f
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-24 21:53:37 +0900

    update

--- room.go
+++ room.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -43,7 +44,19 @@ const (
 
 var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: messageBufferSize}
 
-func (r *room) ServeHTTP(w http.ResponseWriter, reeq *http.Request) {
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	socket, err := upgrader.Upgrade(w, req, nil)
-
+	if err != nil {
+		log.Fatal("ServerHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
 }
