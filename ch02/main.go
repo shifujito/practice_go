261bc2712d1829ca233cf38a1cf585d87487cb71
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-26 00:05:58 +0900

    update

---
+++ ch02/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// 未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		// 成功・ラップされたハンドラをよびだす。
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+
+}
