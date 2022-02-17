322344fcccc0aab1b413e01933083f181f107a8c
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-02-17 21:30:58 +0900

    update

---
+++ webapp/go.mod
@@ -0,0 +1,5 @@
+module sample
+
+go 1.17
+
+require github.com/gorilla/websocket v1.5.0
---
+++ webapp/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// once do は一度だけ呼び出すもの
+	t.once.Do(func() {
+		// htmlファイルをパース(解析)し、
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	// 出力先に渡す。
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8081", "The addr of the application.")
+	flag.Parse() // parse the flags
+
+	// ルームを作成
+	r := newRoom()
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
