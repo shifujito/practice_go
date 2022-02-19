c885a525f03fb7d6b8287cb3a491f22286842ccc
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-02-19 20:20:47 +0900

    update

--- webapp/auth.go
+++ webapp/auth.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 
-	"github.com/stretchr/gomniauth"
+	"golang.org/x/oauth2"
+	google2 "golang.org/x/oauth2/google"
 )
 
 type authHandler struct {
@@ -32,18 +32,22 @@ func MustAuth(handler http.Handler) http.Handler {
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	segs := strings.Split(r.URL.Path, "/")
 	action := segs[2]
-	provider := segs[3]
+	// provider := segs[3]
 	switch action {
 	case "login":
-		provider, err := gomniauth.Provider(provider)
-		if err != nil {
-			log.Fatalln("認証プロバイダーの取得に失敗しました:", provider, "-", err)
+		config := oauth2.Config{
+			ClientID:     "880683417554-lo76iaklpb2vif4j96opbs3fcphat66n.apps.googleusercontent.com",
+			ClientSecret: "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",
+			Endpoint:     google2.Endpoint,
+			RedirectURL:  "http://localhost:8081/auth/callback/google",
+			Scopes:       []string{"openid", "email", "profile"},
 		}
-		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
-		if err != nil {
-			log.Fatalln("GetBeginAuthURLの呼び出しエラー", provider, "-", err)
-		}
-		w.Header().Set("Location", loginUrl)
+		url := config.AuthCodeURL("SECURITY_KEY", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		w.Header().Set("Location", "/")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
