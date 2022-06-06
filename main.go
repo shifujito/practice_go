bde7725ca8259f148cfdd24bdf978d2dd7fed76a
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-06 22:39:56 +0900

    auto commit

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 
 func main() {
-	http.ListenAndServe("", nil)
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: &handler,
+	}
+	server.ListenAndServe()
 }
