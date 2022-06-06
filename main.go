02a5d88700d0aa5bf7afecca931263c2276486b3
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-06 21:45:54 +0900

    auto commit

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
-	}
-	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
-}
+import "net/http"
 
 func main() {
-	var httpServer http.Server
-	http.HandleFunc("/", handler)
-	httpServer.Addr = ":8080"
-	httpServer.ListenAndServe()
+	http.ListenAndServe("", nil)
 }
