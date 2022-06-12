6de671ef2da4b5827835954b1af2a54371c34698
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-12 21:11:19 +0900

    auto commit

--- main.go
+++ main.go
@@ -1,72 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
-type Post struct {
-	User    string
-	Threads []string
-}
-
-func jsonHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	post := &Post{
-		User:    "Shion Fujito",
-		Threads: []string{"1", "2", "3"},
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go web",
+		HttpOnly: true,
 	}
-	json, _ := json.Marshal(post)
-	w.Write(json)
-}
-
-func headers(w http.ResponseWriter, r *http.Request) {
-	h := r.Header
-	fmt.Fprintln(w, h)
-}
-
-func body(w http.ResponseWriter, r *http.Request) {
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-	fmt.Fprintln(w, string(body))
-}
-
-func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
-	if err == nil {
-		data, err := ioutil.ReadAll(file)
-		if err == nil {
-			fmt.Fprintln(w, string(data))
-		}
+	c2 := http.Cookie{
+		Name:     "hoge",
+		Value:    "fuga",
+		HttpOnly: true,
 	}
-	fmt.Fprintln(w, r.PostForm)
-}
-
-func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(501)
-	fmt.Fprintln(w, "not implement")
-}
-
-func headerExample(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Location", "http://google.com")
-	w.WriteHeader(302)
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
 }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/header", headers)
-	http.HandleFunc("/body", body)
-	http.HandleFunc("/process", process)
-	http.HandleFunc("/json", jsonHandler)
-	http.HandleFunc("/writeheader", writeHeaderExample)
-	http.HandleFunc("/red", headerExample)
+	http.HandleFunc("/cookie", setCookie)
 
 	server.ListenAndServe()
 }
