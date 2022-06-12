9790a1041ffc7b356613fd3f473f2428a492aebf
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-12 19:46:03 +0900

    auto commit

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -11,57 +10,57 @@ func headers(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, h)
 }
 
-func body(w http.ResponseWriter, r *http.Request) {
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-	fmt.Fprintln(w, string(body))
-}
+// func body(w http.ResponseWriter, r *http.Request) {
+// 	len := r.ContentLength
+// 	body := make([]byte, len)
+// 	r.Body.Read(body)
+// 	fmt.Fprintln(w, string(body))
+// }
 
-func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
-	if err == nil {
-		data, err := ioutil.ReadAll(file)
-		if err == nil {
-			fmt.Fprintln(w, string(data))
-		}
-	}
-	fmt.Fprintln(w, r.PostForm)
-}
+// func process(w http.ResponseWriter, r *http.Request) {
+// 	r.ParseMultipartForm(1024)
+// 	fileHeader := r.MultipartForm.File["uploaded"][0]
+// 	file, err := fileHeader.Open()
+// 	if err == nil {
+// 		data, err := ioutil.ReadAll(file)
+// 		if err == nil {
+// 			fmt.Fprintln(w, string(data))
+// 		}
+// 	}
+// 	fmt.Fprintln(w, r.PostForm)
+// }
 
-func writeExample(w http.ResponseWriter, r *http.Request) {
-	str := `<html>
-	<head>
-	  <title>Golang</title>
-	</head>
-	<body>
-	  <form action="http://127.0.0.1:8080/process" method="POST" enctype="multipart/form-data">
-		<input type="text" name="hello" value="hoge" />
-		<input type="text" name="post" value="456" />
-		<input type="file" name="uploaded">
-		<input type="submit" />
-	  </form>
-	</body>
-  </html>`
-	w.Write([]byte(str))
-}
+// func writeExample(w http.ResponseWriter, r *http.Request) {
+// 	str := `<html>
+// 	<head>
+// 	  <title>Golang</title>
+// 	</head>
+// 	<body>
+// 	  <form action="http://127.0.0.1:8080/process" method="POST" enctype="multipart/form-data">
+// 		<input type="text" name="hello" value="hoge" />
+// 		<input type="text" name="post" value="456" />
+// 		<input type="file" name="uploaded">
+// 		<input type="submit" />
+// 	  </form>
+// 	</body>
+//   </html>`
+// 	w.Write([]byte(str))
+// }
 
-func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(501)
-	fmt.Fprintln(w, "not implement")
-}
+// func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+// 	w.WriteHeader(501)
+// 	fmt.Fprintln(w, "not implement")
+// }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/header", headers)
-	http.HandleFunc("/body", body)
-	http.HandleFunc("/process", process)
-	http.HandleFunc("/write", writeExample)
-	http.HandleFunc("/writeheader", writeHeaderExample)
+	// http.HandleFunc("/body", body)
+	// http.HandleFunc("/process", process)
+	// http.HandleFunc("/write", writeExample)
+	// http.HandleFunc("/writeheader", writeHeaderExample)
 
 	server.ListenAndServe()
 }
