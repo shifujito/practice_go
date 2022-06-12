291e3da97c4e60ae93b4bcdd4ab965c33f5d8332
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-12 17:25:16 +0900

    auto commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -18,7 +19,15 @@ func body(w http.ResponseWriter, r *http.Request) {
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
 	fmt.Fprintln(w, r.PostForm)
 }
 
